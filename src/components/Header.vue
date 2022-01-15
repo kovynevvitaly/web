990f3b6af617bfb442c3a7307c2f14bcb73a76a5
kovynevvitaly <kovynev_v@mail.ru>
2022-01-15 16:08:41 +0300

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <div class="container top-nav">
+      <nav class="link-list">
+        <router-link :to="{name: 'Home'}" class="logo">PhotoNik</router-link>
+        <router-link :to="{name: 'Home'}" class="nav-link" active-class="link-active">Главная</router-link>
+        <router-link :to="{name: 'Service'}" class="nav-link" active-class="link-active">Услуги</router-link>
+        <router-link :to="{name: 'Portfolio'}" class="nav-link" active-class="link-active">Портфолио</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+header {
+  margin-top: 50px;
+}
+
+.top-nav {
+  display: flex;
+}
+
+.logo {
+  font-family: 'Playfair Display', serif;
+  font-size: 64px;
+  color: #fff;
+}
+
+.link-list {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  width: 100%;
+}
+
+.nav-link {
+  color: #fff;
+}
+
+.nav-link:hover {
+  color: #999;
+}
+
+.link-active {
+  font-weight: 700;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+<div></div>
 </template>
 
 <script>
-
 export default {
   name: 'Home'
 }
 </script>
+
+<style>
+.container {
+  max-width: 1530px;
+  margin: 0 auto;
+}
+
+body {
+  background-color: #000;
+}
+
+#app {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 25px;
+  color: #fff;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
