21c06a0f67f664ad5d7685ad44c27dd3df93d596
kovynevvitaly <kovynev_v@mail.ru>
2022-01-16 14:49:25 +0300

    completed news view

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <article>
+    <div class="article-datetime">{{ article.datetime }}</div>
+    <div class="article-title">{{ article.title }}</div>
+    <div class="article-content">{{ article.content }}</div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "NewsItem",
+  props: ["article"]
+}
+</script>
+
+<style scoped>
+article {
+  margin-left: 45px;
+}
+
+.article-datetime {
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+
+.article-title {
+  margin-bottom: 20px;
+}
+
+.article-content {
+  margin-bottom: 60px;
+}
+</style>
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,14 +1,29 @@
 <template>
   <h1>Новости</h1>
-
+  <NewsItem :article="article"/>
+  <NewsItem :article="article"/>
 </template>
 
 <script>
+import NewsItem from '@/components/NewsItem';
+
 export default {
-  name: "News"
+  name: "News",
+  components: {NewsItem},
+  data: function() {
+    return {
+      article: {
+        title: "Test title",
+        datetime: "16.01.2021",
+        content: "Test content"
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+h1 {
+  text-align: center;
+}
 </style>
